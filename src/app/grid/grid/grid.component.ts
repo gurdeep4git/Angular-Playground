bb39ccf6d8a65532831d78c63500ad64531b612a
Gurdeep Singh <gurdeep.singh@quovantis.com>
2021-07-20 13:00:23 +0530

    Learning - ng-template/let

--- src/app/grid/grid/grid.component.ts
+++ src/app/grid/grid/grid.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { cards } from './grid-mock';
 import { Card } from './../../shared/models/card/card.model';
+
+type Product = {
+  name: string;
+  description: string;
+};
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -8,9 +13,38 @@ import { Card } from './../../shared/models/card/card.model';
 })
 export class GridComponent implements OnInit {
   cards: Card[];
+  currentIndex = 0;
+
+  products: Product[] = [
+    { name: 'PS-4', description: 'The best gaming console' },
+    { name: 'XBOX', description: 'The 2nd best gaming console' },
+  ];
+
   constructor() {
     this.cards = cards;
   }
 
   ngOnInit(): void {}
+
+  getProduct() {
+    return {
+      product: this.products[this.currentIndex],
+    };
+  }
+
+  prevClick() {
+    if (this.currentIndex === 0) {
+      return;
+    }
+    this.currentIndex--;
+    this.getProduct();
+  }
+
+  nextClick() {
+    if (this.currentIndex === this.products.length - 1) {
+      return;
+    }
+    this.currentIndex++;
+    this.getProduct();
+  }
 }
